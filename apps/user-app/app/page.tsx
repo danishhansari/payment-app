f95e90eb674e197bd3459b093076c823c78d6b03
Danish <dan71ish@gmail.com>
2024-11-27 21:31:06 +0530

    bootstrap the project with tailwind best of luck Danish

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,10 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className="text-center bg-red-400">Hi theres</h1>
+   </div>
+  );
+}
